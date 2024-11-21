d1da1a4f668ece09f82f04086cc82f4c8d33c904
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-21 17:26:39 -0400

    Events - Form Event

--- 04-typescript_react/typescript-react/src/tutorials/04-events/index.tsx
+++ 04-typescript_react/typescript-react/src/tutorials/04-events/index.tsx
@@ -1,4 +1,8 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+
+type Person = {
+  name: string;
+};
 
 function Component() {
   const [text, setText] = useState('');
@@ -10,21 +14,37 @@ function Component() {
     setEmail(e.target.value);
   };
 
+  // Form Event
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    // const formData = new FormData(e.target as HTMLFormElement);
+
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    const text = formData.get('text') as string;
+    // const person: Person = { name: text };
+    const person: Person = { name: data.text as string };
+  };
+
   return (
     <section>
       {/* <h2>React & TypScript</h2> */}
       {/* <h2>Events</h2> */}
       <h2>events example</h2>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="form-input mb-1"
+          name="text"
           value={text}
           onChange={(event) => setText(event.target.value)}
         />
         <input
           type="email"
           className="form-input mb-1"
+          name="email"
           value={email}
           onChange={handleChange}
         />
