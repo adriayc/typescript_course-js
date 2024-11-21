4781f8a33f80005fe33339ba557548049652c70b
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-20 23:47:21 -0400

    Tasks Project - Useful Info

--- 03-typescript-fundamentals/typescript-fundamentals/src/tasks.ts
+++ 03-typescript-fundamentals/typescript-fundamentals/src/tasks.ts
@@ -1,1 +1,12 @@
-console.log('tasks');
+// const btn = document.querySelector('.test-btn');
+// btn?.addEventListener;
+
+// if (btn) {
+// }
+
+// const btn = document.querySelector<HTMLButtonElement>('.test-btn')!;
+// btn.addEventListener;
+// btn.disabled = true;
+
+const btn = document.querySelector('.test-btn')! as HTMLButtonElement;
+btn.disabled = true;
