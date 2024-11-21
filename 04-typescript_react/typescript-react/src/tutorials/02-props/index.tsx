06dd87d8df739edecd8085cf17e5eea6f867d946
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-21 15:26:57 -0400

    Props - Type Alias / Interface

--- 04-typescript_react/typescript-react/src/tutorials/02-props/index.tsx
+++ 04-typescript_react/typescript-react/src/tutorials/02-props/index.tsx
@@ -1,11 +1,28 @@
+// Props - Type Alias/Interface
+// Type Alias
+type ComponentProps = {
+  name: string;
+  id: number;
+};
+
+// Interface
+// interface ComponentProps {
+//   name: string;
+//   id: number;
+// }
+
 // Props - inline
-function Component({ name, id }: { name: string; id: number }) {
+// function Component({ name, id }: { name: string; id: number }) {
+function Component({ name, id }: ComponentProps) {
+  // function Component(props: ComponentProps) {
   return (
     <div>
       {/* <h2>React & TypeScript</h2> */}
       {/* <h2>Props</h2> */}
       <h1>Name: {name}</h1>
       <h1>ID: {id}</h1>
+      {/* <h1>Name: {props.name}</h1> */}
+      {/* <h1>ID: {props.id}</h1> */}
     </div>
   );
 }
