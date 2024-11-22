c64f3eb450fbeadd43bb056e021d94394e16ed6d
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-21 23:56:33 -0400

    useReducer - Counter

--- 04-typescript_react/typescript-react/src/tutorials/07-reducer/index.tsx
+++ 04-typescript_react/typescript-react/src/tutorials/07-reducer/index.tsx
@@ -9,28 +9,28 @@ function Component() {
     <div>
       {/* <h2>React & TypeScript</h2> */}
       {/* <h2>Reducer</h2> */}
-      <h2>Count: 0</h2>
-      <h2>Status: Active</h2>
+      <h2>Count: {state.count}</h2>
+      <h2>Status: {state.status}</h2>
 
       <div className="btn-container">
         <button
           type="button"
           className="btn"
-          onClick={() => console.log('increment')}
+          onClick={() => dispatch({ type: 'increment' })}
         >
           Increment
         </button>
         <button
           type="button"
           className="btn"
-          onClick={() => console.log('decrement')}
+          onClick={() => dispatch({ type: 'decrement' })}
         >
           Decrement
         </button>
         <button
           type="button"
           className="btn"
-          onClick={() => console.log('reset')}
+          onClick={() => dispatch({ type: 'reset' })}
         >
           Reset
         </button>
--- 04-typescript_react/typescript-react/src/tutorials/07-reducer/reducer.ts
+++ 04-typescript_react/typescript-react/src/tutorials/07-reducer/reducer.ts
@@ -9,10 +9,25 @@ export const initialState: CounterState = {
   status: 'Pending',
 };
 
+type UpdateCountAction = {
+  type: 'increment' | 'decrement' | 'reset';
+};
+
+type CounterAction = UpdateCountAction;
+
 // Counter reducer
 export const counterReducer = (
   state: CounterState,
-  action: any
+  action: CounterAction
 ): CounterState => {
-  return state;
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + 1 };
+    case 'decrement':
+      return { ...state, count: state.count - 1 };
+    case 'reset':
+      return { ...state, count: 0 };
+    default:
+      return state;
+  }
 };
