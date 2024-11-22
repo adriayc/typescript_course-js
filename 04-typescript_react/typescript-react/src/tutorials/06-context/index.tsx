81c11728be63fc29fb87111662e9069bf08bd9fc
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-21 23:32:00 -0400

    Context API - Theme Example

--- 04-typescript_react/typescript-react/src/tutorials/06-context/index.tsx
+++ 04-typescript_react/typescript-react/src/tutorials/06-context/index.tsx
@@ -16,7 +16,20 @@ function Component() {
   return (
     <div>
       <h2>React & TypeScript</h2>
-      <h2>Context</h2>
+      {/* <h2>Context</h2> */}
+      <button
+        type="button"
+        className="btn btn-center"
+        onClick={() => {
+          if (context.theme === 'dark') {
+            context.setTheme('system');
+            return;
+          }
+          context.setTheme('dark');
+        }}
+      >
+        toggle theme
+      </button>
     </div>
   );
 }
