4a766526dced52262586d0d0067246a3d65754ad
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-22 18:54:54 -0400

    RTK - Setup

--- 04-typescript_react/typescript-react/src/main.tsx
+++ 04-typescript_react/typescript-react/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+// RTK
+import { Provider } from 'react-redux';
+import { store } from './store.ts';
 // React query
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
@@ -10,8 +13,10 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
