0dc17b168605f73294219a7690053cee6a9712e4
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-24 15:35:20 -0400

    Link Component

--- 05-store_app/store-app/src/pages/Cart.tsx
+++ 05-store_app/store-app/src/pages/Cart.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+// Components (Shadcn/ui)
+import { Button } from '@/components/ui/button';
+
 function Cart() {
-  return <h1 className="text-4xl">Cart Page</h1>;
+  return (
+    <div>
+      <h1 className="text-4xl">Cart Page</h1>
+      <Link to="/" className="text-7xl text-red-900">
+        back home
+      </Link>
+      <div>
+        <Button asChild size="lg">
+          <Link to="/">home button</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
