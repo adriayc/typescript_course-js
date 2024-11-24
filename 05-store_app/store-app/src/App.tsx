73c8d4f312ce661728fd741c1841365c8cc6190a
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-24 15:27:33 -0400

    React Router - Setup

--- 05-store_app/store-app/src/App.tsx
+++ 05-store_app/store-app/src/App.tsx
@@ -1,5 +1,4 @@
-// Custom hooks (RTK)
-import { useAppSelector } from './hooks';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 // Pages
 import {
   HomeLayout,
@@ -13,26 +12,18 @@ import {
   Checkout,
   Orders,
 } from './pages';
-// Components (Shadcn/ui)
-import { Button } from '@/components/ui/button';
+
+// Router
+const router = createBrowserRouter([
+  { path: '/', element: <Landing /> },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+]);
 
 function App() {
-  const { name } = useAppSelector((state) => state.userState);
-  console.log(name);
-
-  return (
-    <div>
-      <h1 className="text-7xl font-bold">App</h1>
-      <Button
-        variant="destructive"
-        size="lg"
-        onClick={() => console.log('it worked!!!')}
-      >
-        Click me
-      </Button>
-      <Cart />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
