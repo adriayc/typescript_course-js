3fb8e8081a8b1bdd33bc10cf0afeff3e0d3e7aee
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-25 16:28:08 -0400

    CartButton Component

--- 05-store_app/store-app/src/components/CartButton.tsx
+++ 05-store_app/store-app/src/components/CartButton.tsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'lucide-react';
+// Components (Shadcn/ui)
+import { Button } from './ui/button';
+
 function CartButton() {
-  return <div>CartButton</div>;
+  const numItemsInCart = 5;
+
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link to="/cart">
+        <ShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full w-6 h-6 flex justify-center items-center text-xs">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
 
 export default CartButton;
