d4461861bfcda8a61e314cb9dc6b9c6e15e904d0
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-25 16:53:45 -0400

    ErrorPage Component

--- 05-store_app/store-app/src/App.tsx
+++ 05-store_app/store-app/src/App.tsx
@@ -11,6 +11,7 @@ import {
   Login,
   Checkout,
   Orders,
+  Error,
 } from './pages';
 
 // Router
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
@@ -49,8 +51,8 @@ const router = createBrowserRouter([
       },
     ],
   },
-  { path: '/login', element: <Login /> },
-  { path: '/register', element: <Register /> },
+  { path: '/login', element: <Login />, errorElement: <Error /> },
+  { path: '/register', element: <Register />, errorElement: <Error /> },
 ]);
 
 function App() {
