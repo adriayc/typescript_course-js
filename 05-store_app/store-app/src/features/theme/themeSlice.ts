7bac98cb026d4f2b49fe518b750e8cc6cf48bba3
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-25 12:26:42 -0400

    Theme Slice

--- 05-store_app/store-app/src/features/theme/themeSlice.ts
+++ 05-store_app/store-app/src/features/theme/themeSlice.ts
@@ -1,16 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+// Utils
+import { applyTheme } from '@/utils';
 
 export type Theme = 'dark' | 'light' | 'system';
 
-const initialState = {
-  name: 'theme slice',
+type ThemeState = {
+  theme: Theme;
+};
+
+// Get theme LS
+const initializeTheme = (): Theme => {
+  const theme = (localStorage.getItem('theme') as Theme) || 'system';
+  applyTheme(theme);
+  return theme;
+};
+
+const initialState: ThemeState = {
+  // theme: 'system',
+  theme: initializeTheme(),
 };
 
 const themeSlice = createSlice({
   name: 'theme',
   initialState,
-  reducers: {},
+  reducers: {
+    setTheme: (state, action: PayloadAction<Theme>) => {
+      state.theme = action.payload;
+      applyTheme(action.payload);
+      // LS
+      localStorage.setItem('theme', action.payload);
+    },
+  },
 });
+
+// Actions
+export const { setTheme } = themeSlice.actions;
 
 // Reducer
 export default themeSlice.reducer;
