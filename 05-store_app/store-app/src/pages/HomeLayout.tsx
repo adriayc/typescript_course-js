6cb5cfd0b3c1fa0187c4863d9a3e69e08c4221bf
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-27 17:44:06 -0400

    Global Loading - Setup

--- 05-store_app/store-app/src/pages/HomeLayout.tsx
+++ 05-store_app/store-app/src/pages/HomeLayout.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 // Components
-import { Header, Navbar } from '@/components';
+import { Header, Navbar, Loading } from '@/components';
 
 function HomeLayout() {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading'; // Loading state
+
   return (
     <>
       <Header />
       <Navbar />
       <div className="align-element py-20">
-        <Outlet />
+        {isPageLoading ? <Loading /> : <Outlet />}
       </div>
     </>
   );
