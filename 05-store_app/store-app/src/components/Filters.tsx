c171572928c5d46f2f1b7625347f35f12f197479
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-27 18:54:06 -0400

    Form Select

--- 05-store_app/store-app/src/components/Filters.tsx
+++ 05-store_app/store-app/src/components/Filters.tsx
@@ -5,10 +5,11 @@ import { ProductsResponseWithParams } from '@/utils';
 import { Button } from './ui/button';
 // Components
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 function Filters() {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
-  const { search } = params;
+  const { search, category, company, order } = params;
 
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
@@ -18,6 +19,27 @@ function Filters() {
         name="search"
         label="Search Product"
         defaultValue={search}
+      />
+      {/* Categories */}
+      <FormSelect
+        name="category"
+        label="select category"
+        defaultValue={category}
+        options={meta.categories}
+      />
+      {/* Companies */}
+      <FormSelect
+        name="company"
+        label="select company"
+        defaultValue={company}
+        options={meta.companies}
+      />
+      {/* Order */}
+      <FormSelect
+        name="order"
+        label="order by"
+        defaultValue={order}
+        options={['a-z', 'z-a', 'high', 'low']}
       />
       <Button type="submit" size="sm" className="self-end mb-2">
         Search
