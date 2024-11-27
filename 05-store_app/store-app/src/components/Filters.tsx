14def082e6ecbfc6f0877b9f630633412465ad11
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-27 18:11:11 -0400

    Default Value

--- 05-store_app/store-app/src/components/Filters.tsx
+++ 05-store_app/store-app/src/components/Filters.tsx
@@ -1,15 +1,20 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useLoaderData } from 'react-router-dom';
+// Utils
+import { ProductsResponseWithParams } from '@/utils';
 // Component (Shadcn/ui)
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from './ui/button';
 
 function Filters() {
+  const { meta, params } = useLoaderData() as ProductsResponseWithParams;
+  const { search } = params;
+
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       <div className="mb-2">
         <Label htmlFor="search">Search Product</Label>
-        <Input type="text" name="search" id="search" defaultValue="" />
+        <Input type="text" name="search" id="search" defaultValue={search} />
       </div>
       <Button type="submit" size="sm" className="self-end mb-2">
         Search
--- 05-store_app/store-app/src/pages/Products.tsx
+++ 05-store_app/store-app/src/pages/Products.tsx
@@ -1,6 +1,10 @@
 import { LoaderFunction } from 'react-router-dom';
 // Utils
-import { customFetch, type ProductsResponse } from '@/utils';
+import {
+  customFetch,
+  type ProductsResponse,
+  type ProductsResponseWithParams,
+} from '@/utils';
 // Components
 import { Filters, PaginationContainer, ProductsContainer } from '@/components';
 
@@ -9,8 +13,7 @@ const url = '/products';
 // Loader
 export const loader: LoaderFunction = async ({
   request,
-}): Promise<ProductsResponse> => {
-  // console.log(request.url);
+}): Promise<ProductsResponseWithParams> => {
   // Get query params
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
@@ -20,7 +23,7 @@ export const loader: LoaderFunction = async ({
   const response = await customFetch<ProductsResponse>(url, {
     params,
   });
-  return { ...response.data };
+  return { ...response.data, params };
 };
 
 function Products() {
