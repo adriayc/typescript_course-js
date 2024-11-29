fdd5540dd323edb1053edd60cfffdd40c1003f67
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-29 12:18:25 -0400

    Cart Slice - Setup

--- 05-store_app/store-app/src/features/cart/cartSlice.ts
+++ 05-store_app/store-app/src/features/cart/cartSlice.ts
@@ -1,14 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
+// Utils
+import { type CartState } from '@/utils';
 
-const initialState = {
-  name: 'cart slice',
+const defaultState: CartState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+// Get LS
+const getCartFromLocalStorage = (): CartState => {
+  const cart = localStorage.getItem('cart');
+  return cart ? JSON.parse(cart) : defaultState;
 };
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState,
-  reducers: {},
+  initialState: getCartFromLocalStorage(),
+  reducers: {
+    addItem: () => {},
+    removeItem: () => {},
+    editItem: () => {},
+    clearCart: () => {},
+    calculateTotals: () => {},
+  },
 });
+
+// Actions
+export const { addItem, removeItem, editItem, clearCart, calculateTotals } =
+  cartSlice.actions;
 
 // Reducer
 export default cartSlice.reducer;
--- 05-store_app/store-app/src/utils/types.ts
+++ 05-store_app/store-app/src/utils/types.ts
@@ -51,3 +51,23 @@ export type SingleProductResponse = {
   // meta: {}; // Warning (Empty object)
   meta: object;
 };
+
+export type CartItem = {
+  cartID: string;
+  productID: number;
+  image: string;
+  title: string;
+  price: string;
+  amount: number;
+  productColor: string;
+  company: string;
+};
+
+export type CartState = {
+  cartItems: CartItem[];
+  numItemsInCart: number;
+  cartTotal: number;
+  shipping: number;
+  tax: number;
+  orderTotal: number;
+};
