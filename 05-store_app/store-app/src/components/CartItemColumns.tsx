5121738d1e3268b9330dd27dbec817c6abcb68f9
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-30 00:43:41 -0400

    Cart Items List

--- 05-store_app/store-app/src/components/CartItemColumns.tsx
+++ 05-store_app/store-app/src/components/CartItemColumns.tsx
@@ -1,5 +1,15 @@
-function CartItemColumns() {
-  return <div>CartItemColumns</div>;
-}
+export const FirstColumn = () => {
+  return <h4>First Column</h4>;
+};
 
-export default CartItemColumns;
+export const SecondColumn = () => {
+  return <h4>Second Column</h4>;
+};
+
+export const ThirdColumn = () => {
+  return <h4>Third Column</h4>;
+};
+
+export const FourthColumn = () => {
+  return <h4>Fourth Column</h4>;
+};
