132bda35a7260bff3ce39d4ce13e30daa2b043e2
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-30 17:19:03 -0400

    Register Page Setup

--- 05-store_app/store-app/src/pages/Register.tsx
+++ 05-store_app/store-app/src/pages/Register.tsx
@@ -1,5 +1,37 @@
+import { Form, Link } from 'react-router-dom';
+// Components (Shadcn/ui)
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+// Components
+import { FormInput } from '@/components';
+
 function Register() {
-  return <h1 className="text-4xl">Register Page</h1>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Card className="w-96 bg-muted">
+        <CardHeader>
+          <CardTitle className="text-center">Register</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form>
+            <FormInput type="text" name="username" defaultValue="test" />
+            <FormInput type="email" name="email" defaultValue="test@test.com" />
+            <FormInput type="password" name="password" defaultValue="secret" />
+            {/* Button */}
+            <Button type="submit" className="w-full mt-4">
+              Submit
+            </Button>
+            <p className="text-center mt-4">
+              Already a member?
+              <Button type="button" asChild variant="link">
+                <Link to="/login">Login</Link>
+              </Button>
+            </p>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
 
 export default Register;
