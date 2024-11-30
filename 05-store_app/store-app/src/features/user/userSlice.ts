30e50597f7de6f864f0046f536e47689d5f4ad94
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-30 16:51:15 -0400

    User Slice

--- 05-store_app/store-app/src/features/user/userSlice.ts
+++ 05-store_app/store-app/src/features/user/userSlice.ts
@@ -1,14 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { toast } from '@/hooks/use-toast';
 
-const initialState = {
-  name: 'user slice',
+export type User = {
+  username: string;
+  jwt: string;
+};
+
+type UserState = {
+  user: User | null;
+};
+
+// Get LS
+const getUserFromLocalStorage = (): User | null => {
+  const user = localStorage.getItem('user');
+  if (!user) return null;
+  return JSON.parse(user);
+};
+
+const initialState: UserState = {
+  user: getUserFromLocalStorage(),
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    loginUser: (state, action: PayloadAction<User>) => {
+      const user = action.payload;
+      state.user = user;
+      // Set LS
+      localStorage.setItem('user', JSON.stringify(user));
+      // Toast
+      if (user.username === 'demo user') {
+        toast({ description: 'Welcome Guest User' });
+        return;
+      }
+      toast({ description: 'Login successful' });
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      // Remove LS
+      localStorage.removeItem('user');
+    },
+  },
 });
+
+// Actions
+export const { loginUser, logoutUser } = userSlice.actions;
 
 // Reducer
 export default userSlice.reducer;
