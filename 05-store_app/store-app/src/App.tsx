abc24820c9ef11174541929789ef18202ed96d5a
Adriano Ayala <adriano.ayc@gmail.com>
2024-12-01 12:30:50 -0400

    Checkout Page Setup

--- 05-store_app/store-app/src/App.tsx
+++ 05-store_app/store-app/src/App.tsx
@@ -21,6 +21,7 @@ import { ErrorElement } from './components';
 import { loader as landingLoader } from './pages/Landing';
 import { loader as productsLoader } from './pages/Products';
 import { loader as singleProductLoader } from './pages/SingleProduct';
+import { loader as checkoutLoader } from './pages/Checkout';
 // Actions
 import { action as registerUserAction } from './pages/Register';
 import { action as loginUserAction } from './pages/Login';
@@ -65,6 +66,7 @@ const router = createBrowserRouter([
         path: 'checkout',
         element: <Checkout />,
         errorElement: <ErrorElement />,
+        loader: checkoutLoader(store),
       },
       {
         path: 'orders',
--- 05-store_app/store-app/src/pages/Checkout.tsx
+++ 05-store_app/store-app/src/pages/Checkout.tsx
@@ -1,5 +1,34 @@
+// Custom hooks (RTK)
+import { useAppSelector } from '@/hooks';
+// Store
+import { type ReduxStore } from '@/store';
+// Components
+import { CartTotals, CheckoutForm, SectionTitle } from '@/components';
+import { LoaderFunction } from 'react-router-dom';
+
+// Loader
+export const loader =
+  (store: ReduxStore): LoaderFunction =>
+  async (): Promise<null> => {
+    console.log(store);
+    return null;
+  };
+
 function Checkout() {
-  return <h1 className="text-4xl">Checkout Page</h1>;
+  const cartTotal = useAppSelector((state) => state.cartState.cartTotal);
+
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Place your order" />
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 }
 
 export default Checkout;
