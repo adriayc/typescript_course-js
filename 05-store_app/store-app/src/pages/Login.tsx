1426606db84f2045f14cd1470586b4d3799af8cd
Adriano Ayala <adriano.ayc@gmail.com>
2024-12-01 00:48:51 -0400

    Login Guest User Request

--- 05-store_app/store-app/src/pages/Login.tsx
+++ 05-store_app/store-app/src/pages/Login.tsx
@@ -1,4 +1,12 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useNavigate } from 'react-router-dom';
+import { toast } from '@/hooks/use-toast';
+import { AxiosResponse } from 'axios';
+// Custom hooks (RTK)
+import { useAppDispatch } from '@/hooks';
+// Actions (RTK)
+import { loginUser } from '@/features/user/userSlice';
+// Utils
+import { customFetch } from '@/utils';
 // Components (Shadcn/ui)
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
@@ -6,8 +14,26 @@ import { Button } from '@/components/ui/button';
 import { FormInput, SubmitBtn } from '@/components';
 
 function Login() {
-  const loginAsGuestUser = () => {
-    console.log('Guest user');
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async (): Promise<void> => {
+    try {
+      const response: AxiosResponse = await customFetch.post('/auth/local', {
+        identifier: 'test@test.com',
+        password: 'secret',
+      });
+      const username = response.data.user.username;
+      const jwt = response.data.jwt;
+      // Dispatch
+      dispatch(loginUser({ username, jwt }));
+      // Navigate
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+      // Toast
+      toast({ description: 'Login Failed' });
+    }
   };
 
   return (
